b6d9ea323dc54108e79c7686b12d083038e1b631
colemoebius1 <Guest@WTS6.local>
2015-05-28 16:00:31 -0700

    dwefg

---
+++ src/Validation.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Validation {
+	public static void main(String[] args) {
+
+		// 1. Watch the first 2 minutes of this movie: http://vimeo.com/2485018
+	
+		// 2. On paper, write all the numbers that get printed when you run this class
+		
+		// 3. Use the randomNumber to give the user a random compliment.
+for(int i=0; i <=10; i++)
+{
+	int randomNumber = new Random().nextInt(5);
+	
+	if(randomNumber == 1)
+	{
+		System.out.println("not an fgt");
+	}	
+	if(randomNumber == 2)	
+	{System.out.println("ur ok");
+	}	
+	if(randomNumber == 3)
+	{
+		System.out.println("i can tolerate u");
+	}
+	if(randomNumber == 4)	
+	
+		{System.out.println("ur as fast as sanic");
+		}
+	if(randomNumber == 5)
+	{
+		System.out.println("Ur so dank m8");
+	}
+}
+		// 4. Repeat all the code above 10 times
+		
+		// 5. Find someone to test out your program. They will like it :)
+	}
+}
+
+
